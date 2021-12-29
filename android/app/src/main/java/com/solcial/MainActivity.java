5109cb0f8d5fb8c19e7a553ee6d9c967237726be
aliraza944 <alisabri022@gmail.com>
2021-12-29 10:33:41 +0500

    React navigation, dummy screens
    -Added and configured react navigation

--- android/app/src/main/java/com/solcial/MainActivity.java
+++ android/app/src/main/java/com/solcial/MainActivity.java
@@ -1,7 +1,7 @@
 package com.solcial;
 
 import com.facebook.react.ReactActivity;
-
+import android.os.Bundle;
 public class MainActivity extends ReactActivity {
 
   /**
@@ -12,4 +12,8 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "Solcial";
   }
+  @Override
+protected void onCreate(Bundle savedInstanceState) {
+  super.onCreate(null);
+}
 }
