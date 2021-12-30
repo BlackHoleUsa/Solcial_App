cc43a9f86fcc3968be34186d557013310f490654
aliraza944 <alisabri022@gmail.com>
2021-12-30 11:17:38 +0500

    Added icons for the bottom tabs navigators

--- Navigation/MainApp/index.js
+++ Navigation/MainApp/index.js
@@ -5,7 +5,7 @@ import Products from '../Products';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import {View} from 'react-native';
-
+import Icons from 'react-native-vector-icons/Ionicons';
 import Cart from '../../screens/Cart';
 import Notifications from '../../screens/Notifications';
 import Profile from '../../screens/Profile';
@@ -14,7 +14,19 @@ const Tab = createBottomTabNavigator();
 
 const MainApp = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({color, size}) => {
+          const icons = {
+            Home: 'ios-home-outline',
+            Cart: 'ios-cart-outline',
+            Notifications: 'ios-notifications-outline',
+            Profile: 'ios-person-outline',
+          };
+
+          return <Icons name={icons[route.name]} color="black" size={size} />;
+        },
+      })}>
       <Tab.Screen
         name="Home"
         component={Products}
