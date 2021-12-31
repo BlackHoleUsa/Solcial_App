154a9647d03da32b83273998d6501805f735a532
aliraza944 <alisabri022@gmail.com>
2021-12-31 15:56:02 +0500

    Added the custom component to enter the input with label text

---
+++ components/InputFields/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {View, StyleSheet, TextInput, Text, Platform} from 'react-native';
+
+const InputField = props => {
+  const [text, onChangeText] = React.useState('Solcial app');
+  const [number, onChangeNumber] = React.useState(null);
+  return (
+    <View style={styles.container__main}>
+      <Text style={styles.text__input}>{props.label}</Text>
+      <View style={styles.container__input}>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text}
+          selectionColor="white"
+          placeholder="Email"
+        />
+      </View>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container__main: {
+    width: 300,
+  },
+  container__input: {
+    width: 300,
+    borderWidth: 1,
+    borderColor: '#707070',
+  },
+  input: {
+    height: 20,
+    margin: 10,
+
+    color: 'white',
+    padding: 10,
+    paddingVertical: 0,
+    fontFamily: 'Poppins-Regular',
+  },
+  text__input: {
+    color: 'white',
+    fontFamily: 'Poppins-SemiBold',
+    marginBottom: 5,
+    fontWeight: Platform.OS === 'ios' ? '600' : 'normal',
+  },
+});
+export default InputField;
--- screens/login/index.js
+++ screens/login/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {View, StyleSheet, Platform, Image} from 'react-native';
 import {Text, TouchableRipple, Button} from 'react-native-paper';
@@ -5,6 +6,8 @@ import BackgroundGradient from '../../components/BackgroundGradient';
 import CustomStatusBar from '../../components/CustomStatusBar';
 import useLogin from '../../hooks/useLogin';
 import {Images} from '../../assets/images';
+import InputField from '../../components/InputFields';
+
 const Login = ({navigation}) => {
   const {handleSignupNavigation, handleForogotPasswordNavigation} =
     useLogin(navigation);
@@ -12,36 +15,52 @@ const Login = ({navigation}) => {
   return (
     <BackgroundGradient>
       <CustomStatusBar backgroundColor="black" barStyle="light-content" />
-      <Image source={Images.websiteLogo} />
-      <Text style={styles.text}>Login screen</Text>
-      <Button mode="contained" onPress={() => navigation.navigate('Main App')}>
-        Main App
-      </Button>
-      <TouchableRipple
-        onPress={handleForogotPasswordNavigation}
-        rippleColor="rgba(0, 0, 0, .32)">
-        <Text style={styles.text}>Forgot Password</Text>
-      </TouchableRipple>
-      <TouchableRipple
-        onPress={handleSignupNavigation}
-        rippleColor="rgba(0, 0, 0, .32)">
-        <Text style={styles.text}>Sign up</Text>
-      </TouchableRipple>
+      <View style={styles.login__container}>
+        <Image source={Images.websiteLogo} />
+
+        <InputField label="Email" />
+        <InputField label="Password" />
+        <Button
+          mode="contained"
+          onPress={() => navigation.navigate('Main App')}
+          style={styles.login__button}
+          labelStyle={{color: 'black', fontFamily: 'Poppins-Regular'}}>
+          Login
+        </Button>
+        <TouchableRipple
+          onPress={handleForogotPasswordNavigation}
+          rippleColor="rgba(0, 0, 0, .32)">
+          <Text style={styles.text}>Forgot Password</Text>
+        </TouchableRipple>
+        <TouchableRipple
+          onPress={handleSignupNavigation}
+          rippleColor="rgba(0, 0, 0, .32)">
+          <Text style={styles.text}>Sign up</Text>
+        </TouchableRipple>
+      </View>
     </BackgroundGradient>
   );
 };
 
 const styles = StyleSheet.create({
   login__container: {
-    backgroundColor: 'yellow',
-    flex: 1,
-
     justifyContent: 'center',
     alignItems: 'center',
   },
   text: {
     fontFamily: 'Poppins-ExtraBold',
     fontWeight: '600',
+    color: 'white',
+  },
+  input: {
+    width: 300,
+    backgroundColor: 'black',
+    color: 'white ',
+  },
+  login__button: {
+    marginTop: 10,
+    width: '100%',
+    backgroundColor: 'white',
     color: 'white',
   },
 });
