d71251a2159d38a0f978a15ebadda88989a4dcae
aliraza944 <alisabri022@gmail.com>
2022-01-03 18:16:42 +0500

    Added fontweight for manually setting weight for ios devices

--- screens/Notifications/Styles.js
+++ screens/Notifications/Styles.js
@@ -1,4 +1,16 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Platform} from 'react-native';
+import {fontWeights} from '../../assets/Data';
 export const styles = StyleSheet.create({
-  container__main: {},
+  container__main: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  heading__main: {
+    marginTop: 20,
+    fontFamily: 'Poppins-Medium',
+    fontWeight: Platform.OS === 'ios' ? fontWeights.medium : 'normal',
+    fontSize: 20,
+  },
 });
--- screens/Notifications/index.js
+++ screens/Notifications/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import {StatusBar, View} from 'react-native';
+import {View} from 'react-native';
 import {Text} from 'react-native-paper';
-import {SafeAreaView} from 'react-native-safe-area-context';
+
 import CustomStatusBar from '../../components/CustomStatusBar';
 import {styles} from './Styles';
 import {useIsFocused} from '@react-navigation/native';
 const Notifications = () => {
   const isFocused = useIsFocused();
   return (
-    <View>
+    <View style={styles.container__main}>
       {isFocused && (
         <CustomStatusBar backgroundColor={'white'} barStyle={'dark-content'} />
       )}
-      <Text>Notifications screen</Text>
+      <Text style={styles.heading__main}>Notifications </Text>
     </View>
   );
 };
