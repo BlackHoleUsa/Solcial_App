c6a702538465dfdb2d81644c53f5e644c80f7d0a
aliraza944 <alisabri022@gmail.com>
2022-01-03 11:30:17 +0500

    Signup screen styles
    -Added the backgroubd gradient
    -Added the welcome text
    -Sperated the style and component files

---
+++ screens/signup/Styles.js
@@ -0,0 +1,9 @@
+import {StyleSheet} from 'react-native';
+export const styles = StyleSheet.create({
+  container__main: {},
+  text__welcome: {
+    fontFamily: 'Poppins-SemiBold',
+    color: 'white',
+    fontSize: 20,
+  },
+});
--- screens/signup/index.js
+++ screens/signup/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Text, View} from 'react-native';
-
+import BackgroundGradient from '../../components/BackgroundGradient';
+import {styles} from './Styles';
 const Signup = () => {
   return (
-    <View>
-      <Text>signup screen</Text>
-    </View>
+    <BackgroundGradient>
+      <Text style={styles.text__welcome}>Welocome to SOLEcial app!</Text>
+    </BackgroundGradient>
   );
 };
 
