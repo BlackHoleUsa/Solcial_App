1f6f89a1ba82231e3cadb349060b18e3af6f6ca1
aliraza944 <alisabri022@gmail.com>
2022-01-04 18:20:39 +0500

    Added basic styling to the cart item card

--- screens/Cart/CartItemCard/Styles.js
+++ screens/Cart/CartItemCard/Styles.js
@@ -1,16 +1,53 @@
 import {Platform, StyleSheet} from 'react-native';
-import {fontWeights} from '../../assets/Data';
+import {fontWeights} from '../../../assets/Data';
 export const styles = StyleSheet.create({
-  container__main: {},
-  container__image: {},
-  image: {},
-  container__content: {},
-  text__itemTitle: {},
-  text__size: {},
-  container__priceAndButtons: {},
+  container__main: {
+    width: '100%',
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+
+    borderBottomWidth: 1,
+    borderBottomColor: '#E6E6E6',
+  },
+  container__imageAndContent: {
+    width: '100%',
+    flexDirection: 'row',
+    height: 150,
+    justifyContent: 'space-between',
+  },
+  container__image: {width: '40%'},
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  container__content: {width: '50%'},
+  text__itemTitle: {
+    fontFamily: 'Poppins-Light',
+    fontWeight: fontWeights.regular,
+    color: 'black',
+  },
+  text__size: {color: '#9D9EA3'},
+  container__priceAndButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+
   text__price: {},
-  container__buttons: {},
-  quantity__button: {},
+  container__buttons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderWidth: 1,
+    width: 100,
+    borderColor: 'black',
+    paddingHorizontal: 10,
+  },
+  quantity__button: {
+    padding: 5,
+    margin: 0,
+    textAlign: 'center',
+  },
   text__quantity: {},
   container__removeButton: {},
   remove__button: {},
--- screens/Cart/CartItemCard/index.js
+++ screens/Cart/CartItemCard/index.js
@@ -1,35 +1,34 @@
 import React from 'react';
-import {Text, View, Image} from 'react-native';
+import {Text, View, Image, TouchableOpacity} from 'react-native';
 import {styles} from './Styles';
 import {Images} from '../../../assets/images';
 import {Button} from 'react-native-paper';
+import Icons from 'react-native-vector-icons/AntDesign';
 const CartItemCard = ({navigation}) => {
   return (
     <View style={styles.container__main}>
-      <View style={styles.container__image}>
-        <Image source={Images.cardImage} style={styles.image} />
-      </View>
-      <View style={styles.container__content}>
-        <Text style={styles.text__itemTitle}>Women hoodie</Text>
-        <Text style={styles.text__size}>Size : L</Text>
-        <View style={styles.container__priceAndButtons}>
-          <Text style={styles.text__price}>$50</Text>
-          <View style={styles.container__buttons}>
-            <Button
-              mode="contained"
-              onPress={() => navigation.navigate('Main App')}
-              style={styles.quantity__button}
-              labelStyle={{color: 'black', fontFamily: 'Poppins-Regular'}}>
-              -
-            </Button>
-            <Text style={styles.text__quantity}>1</Text>
-            <Button
-              mode="contained"
-              onPress={() => navigation.navigate('Main App')}
-              style={styles.quantity__button}
-              labelStyle={{color: 'black', fontFamily: 'Poppins-Regular'}}>
-              +
-            </Button>
+      <View style={styles.container__imageAndContent}>
+        <View style={styles.container__image}>
+          <Image source={Images.cardImage} style={styles.image} />
+        </View>
+        <View style={styles.container__content}>
+          <Text style={styles.text__itemTitle}>Women hoodie</Text>
+          <Text style={styles.text__size}>Size : L</Text>
+          <View style={styles.container__priceAndButtons}>
+            <Text style={styles.text__price}>$50</Text>
+            <View style={styles.container__buttons}>
+              <TouchableOpacity
+                style={styles.quantity__button}
+                onPress={() => {}}>
+                <Icons name="minus" size={10} color="black" />
+              </TouchableOpacity>
+              <Text style={styles.text__quantity}>1</Text>
+              <TouchableOpacity
+                style={styles.quantity__button}
+                onPress={() => {}}>
+                <Icons name="plus" size={10} color="black" />
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
       </View>
@@ -38,7 +37,7 @@ const CartItemCard = ({navigation}) => {
           mode="contained"
           onPress={() => navigation.navigate('Main App')}
           style={styles.remove__button}
-          labelStyle={{color: 'black', fontFamily: 'Poppins-Regular'}}>
+          labelStyle={{color: 'black', fontFamily: 'Poppins-Light'}}>
           Remove
         </Button>
       </View>
