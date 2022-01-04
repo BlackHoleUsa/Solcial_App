cebe7aa99333b0839f64da3ae59405fbe99652d9
aliraza944 <alisabri022@gmail.com>
2022-01-04 11:26:07 +0500

    dummy cards added to raffle screen into the scroll view

--- screens/Raffle/index.js
+++ screens/Raffle/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import {View} from 'react-native';
+import {ScrollView, View} from 'react-native';
 import {Text} from 'react-native-paper';
 import {styles} from './Styles';
 import RaffleCard from './RaffleCard';
 const Raffle = () => {
   return (
-    <View style={styles.container__main}>
+    <ScrollView style={styles.container__main}>
       <RaffleCard />
-    </View>
+      <RaffleCard />
+      <RaffleCard />
+      <RaffleCard />
+      <RaffleCard />
+    </ScrollView>
   );
 };
 
