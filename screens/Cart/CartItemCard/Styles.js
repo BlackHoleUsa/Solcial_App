481a50425bbcb51e3eeb1be5bf76d6509efa718a
aliraza944 <alisabri022@gmail.com>
2022-01-04 17:25:08 +0500

    Added the elements for the cart item card

--- screens/Cart/CartItemCard/Styles.js
+++ screens/Cart/CartItemCard/Styles.js
@@ -2,4 +2,16 @@ import {Platform, StyleSheet} from 'react-native';
 import {fontWeights} from '../../assets/Data';
 export const styles = StyleSheet.create({
   container__main: {},
+  container__image: {},
+  image: {},
+  container__content: {},
+  text__itemTitle: {},
+  text__size: {},
+  container__priceAndButtons: {},
+  text__price: {},
+  container__buttons: {},
+  quantity__button: {},
+  text__quantity: {},
+  container__removeButton: {},
+  remove__button: {},
 });
--- screens/Cart/CartItemCard/index.js
+++ screens/Cart/CartItemCard/index.js
@@ -1,11 +1,47 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, Image} from 'react-native';
 import {styles} from './Styles';
-
-const CartItemCard = () => {
+import {Images} from '../../../assets/images';
+import {Button} from 'react-native-paper';
+const CartItemCard = ({navigation}) => {
   return (
     <View style={styles.container__main}>
-      <Text>Cart item</Text>
+      <View style={styles.container__image}>
+        <Image source={Images.cardImage} style={styles.image} />
+      </View>
+      <View style={styles.container__content}>
+        <Text style={styles.text__itemTitle}>Women hoodie</Text>
+        <Text style={styles.text__size}>Size : L</Text>
+        <View style={styles.container__priceAndButtons}>
+          <Text style={styles.text__price}>$50</Text>
+          <View style={styles.container__buttons}>
+            <Button
+              mode="contained"
+              onPress={() => navigation.navigate('Main App')}
+              style={styles.quantity__button}
+              labelStyle={{color: 'black', fontFamily: 'Poppins-Regular'}}>
+              -
+            </Button>
+            <Text style={styles.text__quantity}>1</Text>
+            <Button
+              mode="contained"
+              onPress={() => navigation.navigate('Main App')}
+              style={styles.quantity__button}
+              labelStyle={{color: 'black', fontFamily: 'Poppins-Regular'}}>
+              +
+            </Button>
+          </View>
+        </View>
+      </View>
+      <View style={styles.container__removeButton}>
+        <Button
+          mode="contained"
+          onPress={() => navigation.navigate('Main App')}
+          style={styles.remove__button}
+          labelStyle={{color: 'black', fontFamily: 'Poppins-Regular'}}>
+          Remove
+        </Button>
+      </View>
     </View>
   );
 };
