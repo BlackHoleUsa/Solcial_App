a36e105f9e9a0fff573e31614915a79f6f46bb3e
aliraza944 <alisabri022@gmail.com>
2022-01-04 13:14:11 +0500

    Added the boiler plate code for storecard component

---
+++ components/StoreCard/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {styles} from './Styles';
+const StoreCard = () => {
+  return (
+    <View style={styles.container__main}>
+      <Text>Store card</Text>
+    </View>
+  );
+};
+
+export default StoreCard;
--- screens/StoreScreen/index.js
+++ screens/StoreScreen/index.js
@@ -3,10 +3,12 @@ import {ScrollView, View} from 'react-native';
 import {Text} from 'react-native-paper';
 import {styles} from './Styles';
 import Search from '../../components/Search';
+import StoreCard from '../../components/StoreCard';
 const StoreScreen = () => {
   return (
     <ScrollView style={styles.container__main}>
       <Search />
+      <StoreCard />
     </ScrollView>
   );
 };
