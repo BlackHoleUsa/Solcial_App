96eee3cd3139cd1c0234537afd3a8d8aedcd5233
aliraza944 <alisabri022@gmail.com>
2022-01-04 17:04:00 +0500

    Added the boiler plate for cartitemcar

---
+++ screens/Cart/CartItemCard/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {styles} from './Styles';
+
+const CartItemCard = () => {
+  return (
+    <View style={styles.container__main}>
+      <Text>Cart item</Text>
+    </View>
+  );
+};
+
+export default CartItemCard;
