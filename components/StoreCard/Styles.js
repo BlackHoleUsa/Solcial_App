29c863bd6b74d4f5482231069e92ec1c370748e1
aliraza944 <alisabri022@gmail.com>
2022-01-04 13:57:15 +0500

    Added the styling and elements to the store card

--- components/StoreCard/Styles.js
+++ components/StoreCard/Styles.js
@@ -1,5 +1,30 @@
 import {Platform, StyleSheet} from 'react-native';
 import {fontWeights} from '../../assets/Data';
 export const styles = StyleSheet.create({
-  container__main: {},
+  container__main: {marginBottom: 5, width: 120},
+  container__image: {
+    width: '100%',
+    height: 200,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  text__title: {
+    fontFamily: 'Poppins-Regular',
+    fontWeight: Platform.OS === 'ios' ? fontWeights.regular : 'normal',
+    color: 'black',
+  },
+  container__priceAndCart: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 5,
+  },
+  text__price: {
+    fontFamily: 'Poppins-Medium',
+    fontWeight: Platform.OS === 'ios' ? fontWeights.medium : 'normal',
+    color: 'black',
+  },
 });
--- components/StoreCard/index.js
+++ components/StoreCard/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, Image} from 'react-native';
 import {styles} from './Styles';
+import {Images} from '../../assets/images';
+import Icons from 'react-native-vector-icons/Ionicons';
 const StoreCard = () => {
   return (
     <View style={styles.container__main}>
-      <Text>Store card</Text>
+      <View style={styles.container__image}>
+        <Image
+          source={Images.cardImage}
+          style={styles.image}
+          resizeMode="cover"
+        />
+      </View>
+      <Text style={styles.text__title}>Football shirt</Text>
+      <View style={styles.container__priceAndCart}>
+        <Text style={styles.text__price}>$50</Text>
+        <Icons name="ios-cart-outline" size={25} color="black" />
+      </View>
     </View>
   );
 };
