746af44a835b3891e83dc6a00477b19b37004ced
aliraza944 <alisabri022@gmail.com>
2022-01-05 11:59:59 +0500

    Added the flatlist to cart screen for the display of multiple cart item cards

--- screens/Cart/Styles.js
+++ screens/Cart/Styles.js
@@ -6,11 +6,15 @@ export const styles = StyleSheet.create({
     backgroundColor: 'white',
     justifyContent: 'flex-start',
     alignItems: 'center',
+    paddingHorizontal: 20,
   },
   text__title: {
     fontFamily: 'Poppins-Medium',
     fontSize: 25,
     fontWeight: fontWeights.medium,
     marginTop: 20,
+  },
+  container__flatList: {
+    width: '100%',
   },
 });
--- screens/Cart/index.js
+++ screens/Cart/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import {ScrollView, View} from 'react-native';
+import {FlatList, ScrollView, View} from 'react-native';
 import {Text} from 'react-native-paper';
 import {useIsFocused} from '@react-navigation/native';
 import {styles} from './Styles';
 import CustomStatusBar from '../../components/CustomStatusBar';
 import CartItemCard from './CartItemCard';
+import uuid from 'react-native-uuid';
+
+const cartScreen = [
+  {
+    id: uuid.v4(),
+  },
+  {
+    id: uuid.v4(),
+  },
+];
+
 const Cart = () => {
   const isFocused = useIsFocused();
   return (
@@ -13,7 +24,14 @@ const Cart = () => {
         <CustomStatusBar backgroundColor={'white'} barStyle={'dark-content'} />
       )}
       <Text style={styles.text__title}>Cart</Text>
-      <CartItemCard />
+
+      <View>
+        <FlatList
+          data={cartScreen}
+          keyExtractor={item => item.id}
+          renderItem={({item}) => <CartItemCard />}
+        />
+      </View>
     </View>
   );
 };
