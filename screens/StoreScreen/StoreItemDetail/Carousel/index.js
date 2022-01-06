fedc666e02e10e556ed3151a002090cfb18cf264
aliraza944 <alisabri022@gmail.com>
2022-01-06 11:23:40 +0500

    removed react-native-anchor-carousel and some unnccessary imports

--- screens/StoreScreen/StoreItemDetail/Carousel/index.js
+++ screens/StoreScreen/StoreItemDetail/Carousel/index.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from 'react';
+import React from 'react';
 import {Animated, Image, TouchableOpacity, View} from 'react-native';
 
 import Icons from 'react-native-vector-icons/Ionicons';
--- screens/StoreScreen/StoreItemDetail/index.js
+++ screens/StoreScreen/StoreItemDetail/index.js
@@ -1,11 +1,10 @@
-import React, {useRef} from 'react';
+import React from 'react';
 import {Image, Text, TouchableOpacity, View} from 'react-native';
 import {styles} from './Styles';
 
-import {Dimensions} from 'react-native';
 import {Images} from '../../../assets/images';
 import Carousel from './Carousel';
-const {width: windowWidth} = Dimensions.get('window');
+
 const images = [
   `${Images.cardImage}`,
   `${Images.showImage}`,
@@ -13,7 +12,6 @@ const images = [
   `${Images.large}`,
 ];
 const StoreItemDetail = () => {
-  const ref = useRef(null);
   return (
     <View style={styles.container__main}>
       <Carousel images={images} />
