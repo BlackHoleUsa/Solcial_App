1fd2561eec41826ac840ce9e97c3bba445e3a048
aliraza944 <alisabri022@gmail.com>
2022-01-06 18:21:40 +0500

    completed the styling of the and elements placement of AuctionItemDetails screen

--- screens/Auction/AuctionItemDetail/index.js
+++ screens/Auction/AuctionItemDetail/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import {Text, View} from 'react-native';
-
+import BackgroundVideo from '../../BackgroundVideo';
+import {styles} from './Styles';
+import Icons from 'react-native-vector-icons/Ionicons';
+import RaffleAndAuctionDetailCard from '../../../components/RaffleAndAuctionDetailCard';
 const AuctionItemDetail = () => {
   return (
-    <View>
-      <Text>Auction Item Details</Text>
-    </View>
+    <BackgroundVideo>
+      <View style={styles.container__main}>
+        <View style={styles.container__icon}>
+          <Icons name="arrow-back-circle" size={40} color={'white'} />
+          <Icons name="information-circle-outline" size={40} color={'white'} />
+        </View>
+        <RaffleAndAuctionDetailCard />
+      </View>
+    </BackgroundVideo>
   );
 };
 
