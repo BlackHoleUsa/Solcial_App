7727712d446c45558e4e5177cb0df3850165e8b0
aliraza944 <alisabri022@gmail.com>
2022-01-06 12:42:40 +0500

    Completed the  store item details screen

--- screens/StoreScreen/StoreItemDetail/Styles.js
+++ screens/StoreScreen/StoreItemDetail/Styles.js
@@ -3,19 +3,61 @@ import {fontWeights} from '../../../assets/Data';
 export const styles = StyleSheet.create({
   container__main: {
     width: '100%',
+    flex: 1,
   },
-  carousel: {
+  container__content: {
     width: '100%',
+    padding: 20,
+    justifyContent: 'space-around',
+    flex: 1,
   },
-  container__item: {
-    width: '100%',
+  text__title: {
+    fontFamily: 'Poppins-Medium',
+    fontWeight: fontWeights.medium,
+    color: 'black',
   },
-  container__image: {
-    width: '100%',
-    height: 100,
+  text__description: {
+    fontFamily: 'Poppins-Regular',
+    fontWeight: fontWeights.regular,
+    color: 'black',
   },
-  image__carousel: {
+  text__price: {
+    fontFamily: 'Poppins-Medium',
+    fontWeight: fontWeights.medium,
+    color: 'black',
+    fontSize: 16,
+    marginVertical: 5,
+  },
+  container__size: {
     width: '100%',
-    height: '100%',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  text__size: {
+    fontFamily: 'Poppins-Medium',
+    fontWeight: fontWeights.medium,
+    color: 'black',
+    fontSize: 16,
+  },
+  container__size__buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text__size__button: {
+    marginHorizontal: 10,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    fontFamily: 'Poppins-Medium',
+    fontWeight: fontWeights.medium,
+    color: 'black',
+  },
+  button__checkout: {
+    backgroundColor: 'black',
+    borderRadius: 0,
+    marginBottom: 10,
   },
 });
--- screens/StoreScreen/StoreItemDetail/index.js
+++ screens/StoreScreen/StoreItemDetail/index.js
@@ -4,6 +4,7 @@ import {styles} from './Styles';
 
 import {Images} from '../../../assets/images';
 import Carousel from './Carousel';
+import {Button} from 'react-native-paper';
 
 const images = [
   `${Images.cardImage}`,
@@ -15,6 +16,37 @@ const StoreItemDetail = () => {
   return (
     <View style={styles.container__main}>
       <Carousel images={images} />
+      <View style={styles.container__content}>
+        <Text style={styles.text__title}>Soccer Jacket</Text>
+        <Text style={styles.text__description}>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book.
+        </Text>
+        <Text style={styles.text__price}>Price $50</Text>
+        <View style={styles.container__size}>
+          <Text style={styles.text__size}>Size:</Text>
+          <View style={styles.container__size__buttons}>
+            <TouchableOpacity onPress={() => {}}>
+              <Text style={styles.text__size__button}>40</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => {}}>
+              <Text style={styles.text__size__button}>41</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => {}}>
+              <Text style={styles.text__size__button}>42</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <Button
+          mode="contained"
+          onPress={() => {}}
+          style={styles.button__checkout}
+          labelStyle={{color: 'white', fontFamily: 'Poppins-Regular'}}>
+          Checkout
+        </Button>
+      </View>
     </View>
   );
 };
