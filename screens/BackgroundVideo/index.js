9549555f1e7a21d0d45ce98c4f3797691416e51d
aliraza944 <alisabri022@gmail.com>
2022-01-13 17:57:24 +0500

    change the info.plist file to display the video on the ios

--- screens/BackgroundVideo/index.js
+++ screens/BackgroundVideo/index.js
@@ -6,12 +6,15 @@ const BackgroundVideo = ({children, video}) => {
   return (
     <View style={styles.container__main}>
       <Video
-        source={{uri: video}}
+        source={{
+          uri: video,
+        }}
         muted={true}
         repeat={true}
-        resizeMode={'cover'}
+        resizeMode={'contain'}
         rate={1.0}
         ignoreSilentSwitch={'obey'}
+        fullscreen
         style={styles.container__video}
       />
       {children}
