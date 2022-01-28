36ecb2d1bdc06a95ffdb12da3ba0d338f46c1a77
aliraza944 <alisabri022@gmail.com>
2022-01-28 15:09:53 +0500

    Added the spalsh screen for the android phone

--- android/app/src/main/java/com/solcial/MainActivity.java
+++ android/app/src/main/java/com/solcial/MainActivity.java
@@ -2,6 +2,7 @@ package com.solcial;
 
 import com.facebook.react.ReactActivity;
 import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
   // import com.facebook.react.bridge.JSIModulePackage; // <- add
   // import com.swmansion.reanimated.ReanimatedJSIModulePackage; // <- add
 public class MainActivity extends ReactActivity {
@@ -10,13 +11,15 @@ public class MainActivity extends ReactActivity {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+   
   @Override
   protected String getMainComponentName() {
     return "Solcial";
   }
   @Override
 protected void onCreate(Bundle savedInstanceState) {
-  super.onCreate(null);
+  SplashScreen.show(this,true);
+  super.onCreate(savedInstanceState);
 }
 //  @Override
 //       protected JSIModulePackage getJSIModulePackage() {
